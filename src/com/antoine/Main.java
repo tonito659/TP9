297fca2ed6d19bbb840f0979f6b63e1f3438bd3a
Antoine <fallenhaswon@gmail.com>
2017-01-03 15:03:27 +0100

    passage au dessin
    ajout du gitignore

--- src/com/antoine/Main.java
+++ src/com/antoine/Main.java
@@ -1,10 +1,17 @@
 package com.antoine;
 
+import edu.princeton.cs.introcs.StdDraw;
+
 public class Main {
+
+    public final static int X_MAX= 64;
+    public final static int Y_MAX= 24;
+    public final static float WIDTH= 0.5f;
 
     public static void main(String[] args) {
 	// write your code here
-        cordonnees(7);
+        dessin();
+        //cordonnees(7);
 
     }
     public static void cordonnees(int t){cordonnees(t, 5, 4, 0.07);}
@@ -36,5 +43,28 @@ public class Main {
         System.out.println(" les coordonnées sont en x: "+ pox + " et en y : "+ poy);
 
     }
-}
+
+    public static void dessin(){
+        // hypothèse : on prend
+        double x = 0, y = 20;
+        double vx = 12, vy = 4;
+        int direction = -1;
+        StdDraw.setXscale(-WIDTH, X_MAX + WIDTH);
+        StdDraw.setYscale(-WIDTH, Y_MAX + WIDTH);
+            for (int i = 0; i <= 12000; i++) {
+                x = vx*0.01 + x -0.04*vx;
+                y = (-0.5*9.81*0.01 - vy*0.01 + y)*direction -0.04*vy;
+                vy = vy + 0.01*9.81*-direction;
+                if (y <=0) {
+                    direction = 1;
+                }else direction = -1;
+
+                StdDraw.clear(StdDraw.GRAY);
+                StdDraw.setPenColor(StdDraw.RED);
+                StdDraw.filledCircle(x, y, WIDTH);
+                // display and pause for 20 ms
+                StdDraw.show(20);
+            }
+        }
+    }
 
